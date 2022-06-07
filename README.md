a7cdb6b790e29e404f8480f2e4b7105d6f778da1
chinda trate <chindatrate@me.com>
2022-06-07 17:39:25 -0400

    push changes

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# laughing-computing-machine
+# Mern Book Search
+    
+## Description
+An application that allow users to search for new books to read and save a list of book to purchase.
+
+## Table of Contents
+* [Installation](#installation)
+* [Screenshot](#screenshot)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+Git clone the repository to your local machine. To use application, run the following command: npm install to install necessary dependencies. To start the server locally, run: npm start. Navigate to localhost:3000 in your browser to open the application locally.
+
+## Screenshot
+![Alt text](images/harrypotter.png)
+
+## License
+MIT
+
+## Questions
+Email: chindatrate@me.com
+
+GitHub: https://github.com/chindatrate
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,47 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: "/graphql"
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
+// set new apolloClient
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<SearchBooks />} />
+            <Route exact path='/saved' element={<SavedBooks />} />
+            <Route render={() => <h2>Wrong Page!</h2>} />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
