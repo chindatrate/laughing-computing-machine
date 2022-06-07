307fe3f9b3b557a00a22bc07b59541ada9d620bc
chinda trate <chindatrate@me.com>
2022-06-07 16:51:08 -0400

    update utils

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,11 @@ function App() {
     <Router>
       <>
         <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
       </>
     </Router>
   );
